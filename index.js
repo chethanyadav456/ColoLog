62e9fe28e90126967c751e9ec2c33047cf84e5cf
chethanyadav456 <rockchethan1111@gmail.com>
2023-10-11 09:59:50 +0530

    updated the types and fixed some bugs

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const chalk = require('chalk');
 
+/**
+ * A simple logger utility for logging messages with different log levels.
+ */
 class Logger {
+
     constructor(prefix) {
         this.prefix = prefix || '';
     }
 
+    /**
+     * Log a message at the 'log' level.
+     * @param {string} message - The message to log.
+     */
     log(message) {
         this.printLog('log', message, chalk.green);
     }
 
+    /**
+     * Log a message at the 'info' level.
+     * @param {string} message - The message to log.
+     */
     info(message) {
         this.printLog('info', message, chalk.blue);
     }
 
+    /**
+     * Log a message at the 'warn' level.
+     * @param {string} message - The message to log.
+     */
     warn(message) {
         this.printLog('warn', message, chalk.yellow);
     }
 
+    /**
+     * Log a message at the 'error' level.
+     * @param {string} message - The message to log.
+     */
     error(message) {
         this.printLog('error', message, chalk.red);
     }
